fe9c62d55361498f53056eefe65d2c44a0bc19f7
gipper1998 <trenthalama@gmail.com>
2022-07-11 16:51:03 -0400

    fireworks

---
+++ src/main/java/me/gipper1998/spleef/item/FireworkBuilder.java
@@ -0,0 +1,15 @@
+package me.gipper1998.spleef.item;
+
+import lombok.Getter;
+import org.bukkit.Location;
+import org.bukkit.entity.Firework;
+
+public class FireworkBuilder {
+
+    @Getter
+    private Firework fw;
+
+    public FireworkBuilder(Location location, int power){
+        this.fw = fw;
+    }
+}
